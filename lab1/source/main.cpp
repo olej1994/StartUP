97b01917cd55572596b9e26520e27695d63204c7
Marcin <cola7711@gmail.com>
2018-03-11 22:46:48 +0100

    lab 2

---
+++ lab1/source/main.cpp
@@ -0,0 +1,14 @@
+#include<Windows.h>
+#include "res.h"
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+  HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, NULL);
+  ShowWindow(hwndMainWindow, iCmdShow);
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return 0;
+}
